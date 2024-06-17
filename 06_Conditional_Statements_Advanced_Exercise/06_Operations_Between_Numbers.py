972742509ba632250d0658f828df45585d634afb
programMiro24 <prepodavane.online@gmail.com>
2024-06-17 21:53:42 +0300

    Create 06_Operations_Between_Numbers.py

---
+++ 06_Conditional_Statements_Advanced_Exercise/06_Operations_Between_Numbers.py
@@ -0,0 +1,18 @@
+number_1 = int(input())
+number_2 = int(input())
+operation = input()
+
+text = ''
+# "{N1} {оператор} {N2} = {резултат} - {even/odd}"
+if operation == '+':
+    text=f'{number_1} + {number_2} = {number_1 + number_2} - {even if}'
+elif operation == '-':
+    pass
+elif operation == '*':
+    pass
+elif number_2 == 0:
+    pass
+elif operation == '/':
+    pass
+elif operation == '%':
+    pass
